6ab47b619bc65667c8b3621d2f11e5154a2e7d66
Infection <ltkhiem@apcs.vn>
2018-07-15 08:13:57 +0700

    update recommenda api + stub

--- webapp/database/mongo_helpers.py
+++ webapp/database/mongo_helpers.py
@@ -1,11 +1,16 @@
 from pymongo import MongoClient
+from bson.json_util import dumps
+from .model import job_schema_parser
 
 client = MongoClient('mongodb://localhost:27017/')
 
 db = client['infection']
 
-def get_doc_by_id(list_id):
-    docs = [db['jobs'].find({'id': id }) for id in list_id]
-    print(docs[0])
-    return [db['jobs'].find({'id': id }) for id in list_id]
 
+def get_doc_by_id(list_id):
+    lst_result = [(db['jobs'].find({'id': id})) for id in list_id]
+    #print([item for item in lst_result[1]][0]['skill'])
+    for item in lst_result:
+        print(item[0]['skill'])
+
+    return [job_schema_parser(doc[0]) for doc in lst_result]
