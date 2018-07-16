d642bb7d1a36f43e577f30eec3c9a4b4b596e6e7
minh hoang <minhhoang@minhhoang.local>
2018-07-16 22:49:56 +0700

    Fix upload file

--- frontend/src/containers/home/chatbox/middlewares.js
+++ frontend/src/containers/home/chatbox/middlewares.js
@@ -1,77 +1,80 @@
-import { getAllQuestions, sendMessage, importCV } from './actions'
+import { getAllQuestions, sendMessage, importCV } from './actions';
 
-import { 
+import {
   getAllQuestions as getAllQuestionsRequest,
   sendMessage as sendMessageRequest,
-  importCV as importCVRequest,
-} from '../../../services/homeApi'
+  importCV as importCVRequest
+} from '../../../services/homeApi';
 
- export const onImportCV = (file1, file2, file3) => {
+export const onImportCV = (file1, file2, file3) => {
+  console.log(file1, file2, file3);
   return dispatch => {
-    dispatch(importCV())
+    dispatch(importCV());
     return importCVRequest({ filename: file1, file: file3 })
       .then(res => {
         // console.log('abc', res)
-        return res.json()
+        return res.json();
       })
       .then(data => {
         if (data) {
-          dispatch(importCV.done(data))
+          dispatch(importCV.done(data));
         } else {
-          dispatch(importCV.error({}))
+          dispatch(importCV.error({}));
         }
-      })
-  }
-}
+      });
+  };
+};
 
 export const onGetAllQuestions = () => {
   return dispatch => {
-    dispatch(getAllQuestions())
+    dispatch(getAllQuestions());
     return getAllQuestionsRequest()
       .then(res => {
         // console.log('abc', res)
-        return res.json()
+        return res.json();
       })
       .then(data => {
         if (data) {
-          dispatch(getAllQuestions.done(data))
+          dispatch(getAllQuestions.done(data));
         } else {
-          dispatch(getAllQuestions.error({}))
+          dispatch(getAllQuestions.error({}));
         }
-      })
-  }
-}
+      });
+  };
+};
 
-export const onSendMessage = (msg) => {
+export const onSendMessage = msg => {
   return (dispatch, getState) => {
-    const { messages } = getState('messages').chatboxReducers
-    dispatch(sendMessage(msg))
+    const { messages } = getState('messages').chatboxReducers;
+    dispatch(sendMessage(msg));
     let postMessages = {
       root: [],
       list_message: [],
-      message: msg.message,
-    }
+      message: msg.message
+    };
     if (messages && messages.length > 0) {
-      const oldRoot = messages[messages.length - 1].root ? messages[messages.length - 1].root : []
-      const root = [...oldRoot]
-      root.push(msg.message === 'Có biết' ? 1 : 0)
+      const oldRoot = messages[messages.length - 1].root
+        ? messages[messages.length - 1].root
+        : [];
+      const root = [...oldRoot];
+      root.push(msg.message === 'Có biết' ? 1 : 0);
       postMessages = {
         root: [...root],
         list_message: [],
-        message: msg.message,
-      }
+        message: msg.message
+      };
     }
-    return sendMessageRequest({...postMessages})
+    return sendMessageRequest({ ...postMessages })
       .then(res => {
         // console.log('abc', res)
-        return res.json()
+        return res.json();
       })
       .then(data => {
         if (data) {
-          dispatch(sendMessage.done(data))
+          dispatch(sendMessage.done(data));
         } else {
-          dispatch(sendMessage.error({}))
+          dispatch(sendMessage.error({}));
         }
-      })
-  }
-}
+      });
+  };
+};
